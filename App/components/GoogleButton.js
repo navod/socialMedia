7799d687cb9eb63975a8cda76002239ca47e2339
navod <navodsachintha@gmail.com>
2021-09-14 00:18:24 +0530

    google sign-in done

--- App/components/GoogleButton.js
+++ App/components/GoogleButton.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
+import auth from '@react-native-firebase/auth';
+import {AuthContext} from '../util/AuthContext';
+
 export default function GoogleButton() {
+  const {siginIn} = useContext(AuthContext);
+  GoogleSignin.configure({
+    webClientId:
+      '837677812480-o3ndvrmoj9s48ibmcc8sgu4run1nj3np.apps.googleusercontent.com',
+  });
+
+  const GoogleSignIn = async () => {
+    // Get the users ID token
+    const {idToken, user} = await GoogleSignin.signIn();
+
+    console.log(user);
+    // Create a Google credential with the token
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+    // Sign-in the user with the credential
+    const credential = auth().signInWithCredential(googleCredential);
+
+    if (credential != null) {
+      siginIn('navod');
+    }
+    siginIn;
+  };
+
   return (
     <View>
-      <TouchableOpacity
-        style={styles.btn}
-        onPress={() => console.log('Pressed')}>
+      <TouchableOpacity style={styles.btn} onPress={() => GoogleSignIn()}>
         <Image
           source={require('../assets/icons/google.png')}
           style={{width: '100%', height: '100%'}}
